fe5c0d835736a6fbc4969e2e5dfbe2ee67e2f031
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-15 17:40:13 -0300

    Created project

---
+++ src/main/java/com/project/ProjectAverageHeightApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAverageHeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAverageHeightApplication.class, args);
+	}
+
+}
